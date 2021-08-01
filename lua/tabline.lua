44b5069a8cfc44a793f0f52b1b51a377f570aa4d
Dheepak Krishnamurthy <me@kdheepak.com>
2021-07-31 19:24:19 -0600

    Update files

--- lua/tabline.lua
+++ lua/tabline.lua
@@ -49,13 +49,94 @@ function M.extract_highlight_colors(color_group, scope)
   return color
 end
 
+BufferTab = {}
+function BufferTab:new(self, options)
+  assert(self.bufnr, 'Cannot create BufferTab without bufnr')
+  if options then
+    self.options = options
+  else
+    self.options = M.options
+  end
+  self.file = vim.fn.bufname(self.bufnr)
+  self.buftype = vim.fn.getbufvar(self.bufnr, '&buftype')
+  self.filetype = vim.fn.getbufvar(self.bufnr, '&filetype')
+  self.modified = vim.fn.getbufvar(self.bufnr, '&modified') == 1 and '[+] ' or ''
+  local dev, devhl
+  if self.filetype == 'TelescopePrompt' then
+    dev, devhl = require'nvim-web-devicons'.get_icon('telescope')
+  elseif self.filetype == 'fugitive' then
+    dev, devhl = require'nvim-web-devicons'.get_icon('git')
+  elseif self.filetype == 'vimwiki' then
+    dev, devhl = require'nvim-web-devicons'.get_icon('markdown')
+  elseif self.buftype == 'terminal' then
+    dev, devhl = require'nvim-web-devicons'.get_icon('zsh')
+  else
+    dev, devhl = require'nvim-web-devicons'.get_icon(self.file, vim.fn.expand('#' .. self.bufnr .. ':e'))
+  end
+  self.icon = dev
+  return self
+end
+
+function BufferTab:length(self)
+  local margin = 4
+  return vim.fn.strchars(self.name) + margin
+end
+
+function BufferTab:name(self)
+  if self.buftype == 'help' then
+    return 'help:' .. vim.fn.fnamemodify(self.file, ':t:r')
+  elseif self.buftype == 'quickfix' then
+    return 'quickfix'
+  elseif self.filetype == 'TelescopePrompt' then
+    return 'Telescope'
+  elseif self.file:sub(self.file:len() - 2, self.file:len()) == 'FZF' then
+    return 'FZF'
+  elseif self.buftype == 'terminal' then
+    local _, mtch = string.match(self.file, 'term:(.*):(%a+)')
+    return mtch ~= nil and mtch or vim.fn.fnamemodify(vim.env.SHELL, ':t')
+  elseif self.file == '' then
+    return '[No Name]'
+  end
+  return vim.fn.pathshorten(vim.fn.fnamemodify(self.file, ':p:~:t'))
+end
+
+function BufferTab:render(self)
+  local line = ''
+  line = line .. M.hl(self) .. '%' .. self.bufnr .. '@TablineSwitchBuffer@' .. ' ' .. self.icon .. ' '
+             .. BufferTab:name(self) .. ' ' .. '%T' .. M.buffer_separator(self)
+  return line
+end
+
+function M.buffer_separator(self)
+  local hl = ''
+  if self.current and self.last then
+    hl = '%#tabline_a_to_c#' .. self.options.section_left
+  elseif self.last then
+    hl = '%#tabline_b_to_c#' .. self.options.section_left
+  elseif self.beforecurrent then
+    hl = '%#tabline_b_to_a#' .. self.options.section_left
+  elseif self.current then
+    hl = '%#tabline_a_to_b#' .. self.options.section_left
+  else
+    hl = '%#tabline_a_to_b#' .. self.options.component_left
+  end
+  return hl
+end
+
+function M.buffer_window_count(buffer, opt)
+  local bufnr = buffer.bufnr
+  local nwins = vim.fn.bufwinnr(bufnr)
+  return nwins > 1 and '(' .. nwins .. ') ' or ''
+end
+
 function M.format_buffers(buffers, opt, max_length)
   if max_length == nil then
     max_length = vim.o.columns
   end
   local line = ''
   for i, buffer in pairs(buffers) do
-    line = line .. M.format_buffer(buffer, opt)
+    dump(buffer)
+    line = line .. BufferTab:render(buffer)
   end
   return line
 end
@@ -88,7 +169,7 @@ function M.buffers(opt)
   local buffers = {}
   for b = 1, vim.fn.bufnr('$') do
     if vim.fn.buflisted(b) ~= 0 and vim.fn.getbufvar(b, '&buftype') ~= 'quickfix' then
-      buffers[#buffers + 1] = { bufnr = b }
+      buffers[#buffers + 1] = BufferTab:new{ bufnr = b }
     end
   end
   local line = ''
@@ -117,92 +198,12 @@ function M.buffers(opt)
   return line
 end
 
-function M.buffer_name(buffer, opt)
-  local bufnr = buffer.bufnr
-  local file = vim.fn.bufname(bufnr)
-  local buftype = vim.fn.getbufvar(bufnr, '&buftype')
-  local filetype = vim.fn.getbufvar(bufnr, '&filetype')
-  if buftype == 'help' then
-    return 'help:' .. vim.fn.fnamemodify(file, ':t:r')
-  elseif buftype == 'quickfix' then
-    return 'quickfix'
-  elseif filetype == 'TelescopePrompt' then
-    return 'Telescope'
-  elseif file:sub(file:len() - 2, file:len()) == 'FZF' then
-    return 'FZF'
-  elseif buftype == 'terminal' then
-    local _, mtch = string.match(file, 'term:(.*):(%a+)')
-    return mtch ~= nil and mtch or vim.fn.fnamemodify(vim.env.SHELL, ':t')
-  elseif file == '' then
-    return '[No Name]'
-  end
-  return vim.fn.pathshorten(vim.fn.fnamemodify(file, ':p:~:t'))
-end
-
-function M.buffer_modified(buffer, opt)
-  local bufnr = buffer.bufnr
-  return vim.fn.getbufvar(bufnr, '&modified') == 1 and '[+] ' or ''
-end
-
-function M.buffer_devicon(buffer, opt)
-  local dev, devhl
-  local bufnr = buffer.bufnr
-  local file = vim.fn.bufname(bufnr)
-  local buftype = vim.fn.getbufvar(bufnr, '&buftype')
-  local filetype = vim.fn.getbufvar(bufnr, '&filetype')
-  if filetype == 'TelescopePrompt' then
-    dev, devhl = require'nvim-web-devicons'.get_icon('telescope')
-  elseif filetype == 'fugitive' then
-    dev, devhl = require'nvim-web-devicons'.get_icon('git')
-  elseif filetype == 'vimwiki' then
-    dev, devhl = require'nvim-web-devicons'.get_icon('markdown')
-  elseif buftype == 'terminal' then
-    dev, devhl = require'nvim-web-devicons'.get_icon('zsh')
-  else
-    dev, devhl = require'nvim-web-devicons'.get_icon(file, vim.fn.expand('#' .. bufnr .. ':e'))
-  end
-  if dev then
-    return ' ' .. dev .. ' '
-  else
-    return ' '
-  end
-end
-
-function M.buffer_separator(buffer, opt)
-  local hl = ''
-  if buffer.current and buffer.last then
-    hl = '%#tabline_a_to_c#' .. opt.section_left
-  elseif buffer.last then
-    hl = '%#tabline_b_to_c#' .. opt.section_left
-  elseif buffer.beforecurrent then
-    hl = '%#tabline_b_to_a#' .. opt.section_left
-  elseif buffer.current then
-    hl = '%#tabline_a_to_b#' .. opt.section_left
-  else
-    hl = '%#tabline_a_to_b#' .. opt.component_left
-  end
-  return hl
-end
-
-function M.buffer_window_count(buffer, opt)
-  local bufnr = buffer.bufnr
-  local nwins = vim.fn.bufwinnr(bufnr)
-  return nwins > 1 and '(' .. nwins .. ') ' or ''
-end
-
 function M.hl(buffer, opt)
   if buffer.current then
     return '%#lualine_a_normal#'
   else
     return '%#lualine_b_normal#'
   end
-end
-
-function M.format_buffer(buffer, opt)
-  local line = ''
-  line = line .. M.hl(buffer, opt) .. '%' .. buffer.bufnr .. '@TablineSwitchBuffer@' .. M.buffer_devicon(buffer, opt)
-             .. M.buffer_name(buffer, opt) .. ' ' .. '%T' .. M.buffer_separator(buffer, opt)
-  return line
 end
 
 function M.switch_buffer(bufnr)
